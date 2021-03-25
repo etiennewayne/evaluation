40974af6675e9509d1021b42bffa224c6bd5add2
etiennewayne <62062444+etiennewayne@users.noreply.github.com>
2021-03-25 18:35:38 +0800

    update eval

--- public/js/app.js
+++ public/js/app.js
@@ -2991,11 +2991,55 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: ['code'],
   data: function data() {
     return {
-      data: {}
+      data: {},
+      aydesc: '',
+      instructor: '',
+      institute: '',
+      noOfStudent: ''
     };
   },
   methods: {
@@ -3003,7 +3047,13 @@ __webpack_require__.r(__webpack_exports__);
       var _this = this;
 
       axios.get('/ajax/faculty-rating?code=' + this.code).then(function (res) {
-        _this.data = res.data;
+        if (res.data.length > 0) {
+          _this.data = res.data;
+          _this.aydesc = _this.data[0].ay_desc;
+          _this.instructor = _this.data[0].InsLName + ', ' + _this.data[0].InsFName + ' ' + _this.data[0].InsMName;
+          _this.institute = _this.data[0].InsDept;
+          _this.noOfStudent = _this.data[0].no_of_raters + '/' + _this.data[0].no_students;
+        }
       });
     }
   },
@@ -21441,6 +21491,25 @@ exports.push([module.i, "\n.alert-danger[data-v-b92cab3e]{\n    color: red;\n   
 
 /***/ }),
 
+/***/ "./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css&":
+/*!****************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--6-1!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--6-2!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css& ***!
+  \****************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../../../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "\n.header[data-v-30c740f3]{\n    display: flex;\n    justify-content: center;\n    margin-bottom: 5px;\n}\n.header-text[data-v-30c740f3]{\n    text-align: center;\n    font-weight: bold;\n}\n.school-header[data-v-30c740f3]{\n    display: flex;\n    justify-content: center;\n    flex-direction: column;\n}\n.mybox[data-v-30c740f3]{\n    width: 100%;\n}\n.rating-table > th[data-v-30c740f3]{\n    font-size: 8px;\n}\n@media only screen and (max-width: 640px) {\nbody[data-v-30c740f3] {\n        background-color: lightblue;\n}\n}\n\n\n\n", ""]);
+
+// exports
+
+
+/***/ }),
+
 /***/ "./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/administrator/report/FacultyReport.vue?vue&type=style&index=0&id=2744410a&scoped=true&lang=css&":
 /*!****************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/css-loader??ref--6-1!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--6-2!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/administrator/report/FacultyReport.vue?vue&type=style&index=0&id=2744410a&scoped=true&lang=css& ***!
@@ -22099,6 +22168,36 @@ options.transform = transform
 options.insertInto = undefined;
 
 var update = __webpack_require__(/*! ../../../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
+/***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css&":
+/*!********************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/style-loader!./node_modules/css-loader??ref--6-1!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--6-2!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css& ***!
+  \********************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../../../../../node_modules/css-loader??ref--6-1!../../../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../../node_modules/postcss-loader/src??ref--6-2!../../../../../node_modules/vue-loader/lib??vue-loader-options!./FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css& */ "./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css&");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../../../../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
 
 if(content.locals) module.exports = content.locals;
 
@@ -24633,38 +24732,137 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c("div", [
+    _c("section", { staticClass: "section" }, [
+      _c("div", { staticClass: "container" }, [
+        _vm._m(0),
+        _vm._v(" "),
+        _c("div", { staticClass: "school-header mt-3 mb-5" }, [
+          _c("div", { staticClass: "header-text" }, [
+            _vm._v("TEACHER PERFORMANCE EVALUATION RESULT")
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "header-text" }, [
+            _vm._v(_vm._s(this.aydesc))
+          ])
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "mybox" }, [
+          _c("div", { staticClass: "is-flex is-flex-direction-column" }, [
+            _c("div", [_vm._v(" INSTRUCTOR :" + _vm._s(this.instructor))]),
+            _vm._v(" "),
+            _c("div", [_vm._v("INSTITUTE: " + _vm._s(this.institute))]),
+            _vm._v(" "),
+            _c("div", [_vm._v("NO OF STUDENTS: " + _vm._s(this.noOfStudent))])
+          ])
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "mybox" }, [
+          _c("div", { staticClass: "is-flex is-flex-direction-column" }, [
+            _c(
+              "table",
+              {
+                staticClass:
+                  "table is-bordered is-striped is-narrow is-hoverable is-fullwidth rating-table"
+              },
+              [
+                _vm._m(1),
+                _vm._v(" "),
+                _c(
+                  "tbody",
+                  _vm._l(this.data, function(item) {
+                    return _c("tr", { key: item.id }, [
+                      _c("td", [_vm._v(_vm._s(item.SubjName))]),
+                      _vm._v(" "),
+                      _c("td", [_vm._v(_vm._s(item.no_of_raters))]),
+                      _vm._v(" "),
+                      _c("td", [
+                        _vm._v(
+                          _vm._s(Math.round(item.course_design * 100) / 100)
+                        )
+                      ]),
+                      _vm._v(" "),
+                      _c("td", [
+                        _vm._v(_vm._s(Math.round(item.content * 100) / 100))
+                      ]),
+                      _vm._v(" "),
+                      _c("td", [
+                        _vm._v(_vm._s(Math.round(item.process * 100) / 100))
+                      ]),
+                      _vm._v(" "),
+                      _c("td", [
+                        _vm._v(_vm._s(Math.round(item.outcomes * 100) / 100))
+                      ]),
+                      _vm._v(" "),
+                      _c("td", [
+                        _vm._v(
+                          _vm._s(Math.round(item.personal_quality * 100) / 100)
+                        )
+                      ]),
+                      _vm._v(" "),
+                      _c("td", [
+                        _vm._v(
+                          _vm._s(Math.round(item.avg_category * 100) / 100)
+                        )
+                      ])
+                    ])
+                  }),
+                  0
+                )
+              ]
+            )
+          ])
+        ])
+      ])
+    ])
+  ])
 }
 var staticRenderFns = [
   function() {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [
-      _c("section", { staticClass: "section" }, [
-        _c("div", { staticClass: "container" }, [
-          _c("div", { staticClass: "header" }, [
-            _c("div", { staticClass: "logo" }, [
-              _c("img", { attrs: { src: "" } })
-            ]),
-            _vm._v(" "),
-            _c("div", { staticClass: "school-header" }, [
-              _c("div", { staticClass: "header-text" }),
-              _vm._v(" "),
-              _c("div", { staticClass: "address-text" })
-            ])
-          ]),
-          _vm._v(" "),
-          _c("div", { staticClass: "columns" }, [
-            _c("div", { staticClass: "column" }, [
-              _c("div", { staticClass: "box" }, [
-                _vm._v(
-                  "\n                        INSTRUCTOR\n                    "
-                )
-              ])
-            ])
-          ])
+    return _c("div", { staticClass: "header" }, [
+      _c("div", { staticClass: "logo" }, [
+        _c("img", { attrs: { src: "/img/logo_small.png", width: "70" } })
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "school-header" }, [
+        _c("div", { staticClass: "header-text" }, [
+          _vm._v(
+            "\n                        GOV. ALFONSO D. TAN COLLEGE\n                    "
+          )
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "address-text" }, [
+          _vm._v(
+            "\n                        Maloro, Tangub City, Misamis Occidental\n                    "
+          )
         ])
+      ])
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("thead", [
+      _c("tr", [
+        _c("th", { attrs: { width: "150" } }, [_vm._v("Course")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("No of Raters")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Course Design")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Content")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Process")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Outcomes")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Personal Qualities and Professionalism")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Assessment")])
       ])
     ])
   }
@@ -39106,7 +39304,9 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _FacultyRating_vue_vue_type_template_id_30c740f3_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./FacultyRating.vue?vue&type=template&id=30c740f3&scoped=true& */ "./resources/js/components/administrator/report/FacultyRating.vue?vue&type=template&id=30c740f3&scoped=true&");
 /* harmony import */ var _FacultyRating_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./FacultyRating.vue?vue&type=script&lang=js& */ "./resources/js/components/administrator/report/FacultyRating.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _FacultyRating_vue_vue_type_style_index_0_id_30c740f3_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css& */ "./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
 
 
 
@@ -39114,7 +39314,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__["default"])(
   _FacultyRating_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _FacultyRating_vue_vue_type_template_id_30c740f3_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
   _FacultyRating_vue_vue_type_template_id_30c740f3_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
@@ -39143,6 +39343,22 @@ component.options.__file = "resources/js/components/administrator/report/Faculty
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_FacultyRating_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../../node_modules/babel-loader/lib??ref--4-0!../../../../../node_modules/vue-loader/lib??vue-loader-options!./FacultyRating.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/administrator/report/FacultyRating.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_FacultyRating_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css&":
+/*!*********************************************************************************************************************************!*\
+  !*** ./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css& ***!
+  \*********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_6_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_2_node_modules_vue_loader_lib_index_js_vue_loader_options_FacultyRating_vue_vue_type_style_index_0_id_30c740f3_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../../node_modules/style-loader!../../../../../node_modules/css-loader??ref--6-1!../../../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../../node_modules/postcss-loader/src??ref--6-2!../../../../../node_modules/vue-loader/lib??vue-loader-options!./FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css& */ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/administrator/report/FacultyRating.vue?vue&type=style&index=0&id=30c740f3&scoped=true&lang=css&");
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_6_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_2_node_modules_vue_loader_lib_index_js_vue_loader_options_FacultyRating_vue_vue_type_style_index_0_id_30c740f3_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_6_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_2_node_modules_vue_loader_lib_index_js_vue_loader_options_FacultyRating_vue_vue_type_style_index_0_id_30c740f3_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_6_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_2_node_modules_vue_loader_lib_index_js_vue_loader_options_FacultyRating_vue_vue_type_style_index_0_id_30c740f3_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_6_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_2_node_modules_vue_loader_lib_index_js_vue_loader_options_FacultyRating_vue_vue_type_style_index_0_id_30c740f3_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
 
 /***/ }),
 
--- resources/js/components/administrator/report/FacultyRating.vue
+++ resources/js/components/administrator/report/FacultyRating.vue
@@ -5,24 +5,64 @@
 
                 <div class="header">
                     <div class="logo">
-                        <img src="">
+                        <img src="/img/logo_small.png" width="70">
                     </div>
                     <div class="school-header">
                         <div class="header-text">
-
+                            GOV. ALFONSO D. TAN COLLEGE
                         </div>
                         <div class="address-text">
-                            
+                            Maloro, Tangub City, Misamis Occidental
                         </div>
                     </div>
                 </div>
-                <div class="columns">
-                    <div class="column">
-                        <div class="box">
-                            INSTRUCTOR
-                        </div>
+
+                <div class="school-header mt-3 mb-5">
+                    <div class="header-text">TEACHER PERFORMANCE EVALUATION RESULT</div>
+                    <div class="header-text">{{ this.aydesc}}</div>
+                </div>
+
+                <div class="mybox">
+                    <div class="is-flex is-flex-direction-column">
+                        <div> INSTRUCTOR :{{this.instructor }}</div>
+                        <div>INSTITUTE: {{ this.institute }}</div>
+                        <div>NO OF STUDENTS: {{ this.noOfStudent }}</div>
+                    </div>
+
+                </div>
+
+                <div class="mybox">
+                    <div class="is-flex is-flex-direction-column">
+                        <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth rating-table">
+                            <thead>
+                                <tr>
+                                    <th width="150">Course</th>
+                                    <th>No of Raters</th>
+                                    <th>Course Design</th>
+                                    <th>Content</th>
+                                    <th>Process</th>
+                                    <th>Outcomes</th>
+                                    <th>Personal Qualities and Professionalism</th>
+                                    <th>Assessment</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="item in this.data" :key="item.id">
+                                    <td>{{ item.SubjName }}</td>
+                                    <td>{{ item.no_of_raters }}</td>
+                                    <td>{{ Math.round(item.course_design * 100) / 100 }}</td>
+                                    <td>{{ Math.round(item.content * 100) / 100 }}</td>
+                                    <td>{{ Math.round(item.process * 100) / 100 }}</td>
+                                    <td>{{ Math.round(item.outcomes * 100)/ 100 }}</td>
+                                    <td>{{ Math.round(item.personal_quality * 100) / 100 }}</td>
+                                    <td>{{ Math.round(item.avg_category * 100) / 100 }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
+
+
             </div>
         </section>
     </div>
@@ -34,13 +74,28 @@ export default {
     data(){
         return{
             data: {},
+            aydesc: '',
+
+            instructor: '',
+            institute: '',
+            noOfStudent: '',
+
 
         }
     },
     methods: {
         getRating(){
             axios.get('/ajax/faculty-rating?code='+this.code).then(res=>{
-                this.data = res.data;
+
+                if(res.data.length > 0){
+                    this.data = res.data;
+
+                    this.aydesc = this.data[0].ay_desc;
+                    this.instructor = this.data[0].InsLName + ', ' + this.data[0].InsFName + ' ' + this.data[0].InsMName;
+                    this.institute = this.data[0].InsDept;
+
+                    this.noOfStudent = this.data[0].no_of_raters + '/'+this.data[0].no_students;
+                }
             })
         }
     },
@@ -51,5 +106,37 @@ export default {
 </script>
 
 <style scoped>
+    .header{
+        display: flex;
+        justify-content: center;
+        margin-bottom: 5px;
+    }
+    .header-text{
+        text-align: center;
+        font-weight: bold;
+    }
+
+    .school-header{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+
+    }
+
+    .mybox{
+        width: 100%;
+    }
+
+    .rating-table > th{
+        font-size: 8px;
+    }
+
+    @media only screen and (max-width: 640px) {
+        body {
+            background-color: lightblue;
+        }
+    }
+
+
 
 </style>
